d434b274a644d9590d008517c8f30a655231ab3e
Ali Saferzade <7n0mn2a@code.edu.az>
2023-03-06 01:05:13 +0400

    Task

---
+++ Tasks/AliTap/index.js
@@ -0,0 +1,22 @@
+//Task 3
+// var num = parseInt(prompt("Ededi daxil edin"));
+// num=(num-(num%10))/10
+// num=(num-(num%10))/10
+// alert(num)
+
+
+//Task 4
+// var a, b, c;
+// a = parseInt(prompt("1 ci ededi daxil edin"));
+// b = parseInt(prompt("2 ci ededi daxil edin"));;
+// c = parseInt(prompt("3 cu ededi daxil edin"));;
+
+// if(a>b && b>c){
+//     alert(a)
+// } else if(b>a && a>c){
+//     alert(b)
+// } else if(c>b && b>a){
+//     alert(c)
+// } else{
+//     alert('ededler beraberdir')
+// }
---
+++ Tasks/check18Age/Check18Age/Check18Age/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Check18Age
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Doğum tarixinizi daxil edin");
+            int date = Convert.ToInt32(Console.ReadLine());
+            if (date > 0 && date <= 2023)
+            {
+                if (date + 18 <= 2023)
+                {
+                    Console.WriteLine("Sizin 18 yaşınız tamam olub");
+                }
+                else
+                {
+                    Console.WriteLine("Sizin 18 yaşınız tamam olmayıb");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Tarix mənfi və ya 0 ola bilməz və ya 2023 dən böyük ola bilməz");
+            }
+            Console.Write("Ededi daxil edin: ");
+            int eded = Convert.ToInt32(Console.ReadLine());
+            int sum = 0;
+            while (eded > 0)
+            {
+                sum += eded % 10;
+                eded = (eded - eded % 10) / 10;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
